782aae04db5745128f93eeb05ca7bc65b5a9704f
rdotzlaw <93158008+rdotzlaw@users.noreply.github.com>
2023-04-12 19:40:19 -0500

    added all optimizations and optimized queries

--- implementation.py
+++ implementation.py
@@ -35,35 +35,35 @@ queries = [
 
 
 indexes = [
-    "", # Optimization for Query 1
-    "", # Optimization for Query 2
+    "CREATE INDEX idx_b_2020 ON REQUESTS(created_date, city) WHERE city LIKE "%B" AND created_date > '2020-01-01%';", # Optimization for Query 1
+    "CREATE INDEX idx_agency_nypd ON REQUESTS (agency) WHERE agency IS 'NYPD';", # Optimization for Query 2
     "create index ind_IncidentAddress on REQUESTS (IncidentAddress)", # Optimization 3
     "create index ind_Agency_Closed on REQUESTS (Agency, ClosedDate) where Agency is not ‘NYPD’ and ClosedDate like ‘12-25-%’", # Optimization 4
-    "", # Optimization for Query 5
-    "", # Optimization for Query 6
-    "", # Optimization for Query 7
-    "", # Optimization for Query 8
-    "", # Optimization for Query 9
-    "", # Optimization for Query 10
-    "", # Optimization for Query 11
-    "" # Optimization for Query 12
+    "CREATE INDEX ParkBoroughIndex ON REQUESTS(ParkBorough);", # Optimization for Query 5
+    "CREATE INDEX idx_party_zip ON REQUESTS(incident_zip, descriptor) WHERE incident_zip LIKE '10%' AND (descriptor LIKE '%Party%' OR descriptor LIKE '%party%');", # Optimization for Query 6
+    "Create INDEX idx_ComplaintType ON REQUESTS(ComplaintType);", # Optimization for Query 7
+    "Create INDEX idx_City ON REQUESTS (City);", # Optimization for Query 8
+    "CREATE INDEX idx_CreatedDate ON REQUESTS(CreatedDate);", # Optimization for Query 9
+    "CREATE INDEX CreatedDateIndex ON REQUESTS(CreatedDate) WHERE CreatedDate BETWEEN "2012-10-10" AND "2012-11-03";", # Optimization for Query 10
+    "CREATE TABLE NewYork311Sorted AS SELECT * FROM REQUESTS ORDER BY CreatedDate", # Optimization for Query 11 and 12
+    "CREATE INDEX CreatedDateIndex ON NewYork311Sorted(CreatedDate);" # Optimization for Query 11 and 12
     
 ]
 
 
 optimized_queries = [
-    "", #Query 1
-    "", #Query 2
+    "SELECT complaint_type, COUNT(*) AS total_complaints, ROUND((COUNT(CASE WHEN closed_date IS NOT NULL AND closed_date <> '' THEN 1 END) * 100.0 / COUNT(*)), 2) AS closed_percentage FROM requests_test INDEXED BY idx_b_2020 WHERE city LIKE 'B%' AND created_date > '2020-01-01' GROUP BY complaint_type ORDER BY total_complaints DESC LIMIT 10", #Query 1
+    "SELECT strftime('%Y', date) AS year, strftime('%m', date) AS month, max(request_count) AS max_requests FROM ( SELECT substr(created_date, 1,10) AS date, count(*) AS request_count FROM REQUESTS INDEXED BY idx_agency_nypd WHERE agency IS 'NYPD' GROUP BY date HAVING request_count > 2000) AS request_counts GROUP BY year, month;", # Query 2
     "select IncidentAddress, count(IncidentAddress) from REQUESTS Indexed by ind_IncidentAddress group by IncidentAddress order by count(IncidentAddress) desc limit 10", #Query 3
     "select Agency, AgencyName, count(Agency) as 'Requests Closed' from REQUESTS indexed by ind_Agency_Closed where ClosedDate like "12-25-%" and Agency is not 'NYPD' group by Agency order by count(Agency) desc limit 1", # Query 4
-    "", #Query 5
-    "", #Query 6
-    "", #Query 7
-    "", #Query 8
-    "", #Query 9
-    "", #Query 10
-    "", #Query 11
-    "" #Query 12
+    "SELECT ComplaintType, count() as count FROM REQUESTS WHERE ComplaintType LIKE'%Heat%' AND ParkBorough LIKE '%BROOKLYN%'", #Query 5
+    "SELECT city, descriptor AS description, COUNT(*) AS count FROM REQUESTS INDEXED BY idx_party_zip WHERE incident_zip LIKE '10%' AND (descriptor LIKE '%Party%' OR descriptor LIKE '%party%') GROUP BY city, description ORDER BY count DESC;", #Query 6
+    "SELECT ComplaintType, count() as count FROM REQUESTS GROUP BY ComplaintType ORDER BY count DESC LIMIT 5;", #Query 7
+    "SELECT city, count() from REQUESTS where complaint_type LIKE ‘%Noise%’ GROUP BY city ORDER BY DESC LIMIT 1;", #Query 8
+    "SELECT City, COUNT(*) as count from REQUESTS where CreatedDate  BETWEEN '2020-01-01' and '2022-01-01' GROUP BY City ORDER BY count ASC LIMIT 4;", #Query 9
+    "SELECT COUNT(*) as count, ComplaintType as type FROM NewYork311 WHERE julianday(strftime('%Y-%m-%d', substr(CreatedDate, 1, 10))) >= julianday('2012-10-10') AND julianday(strftime('%Y-%m-%d', substr(CreatedDate, 1, 10))) <= julianday('2012-11-03') group by type order by count desc", #Query 10
+    "SELECT CreatedDate, count() as count FROM NewYork311Sorted WHERE ComplaintType LIKE '%Noise%' GROUP BY CreatedDate ORDER BY count DESC LIMIT 3;", #Query 11
+    "SELECT ComplaintType, Descriptor FROM NewYork311Sorted WHERE CreatedDate LIKE '%2012-12-21%';" #Query 12
 ]
 
 # Loop through the queries and run them
